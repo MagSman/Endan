3596a998ed1f704274d543574d81faaaa4af7f35
shijianghan <250135506@qq.com>
2018-04-10 21:41:53 +0800

    home up

--- app/src/main/java/com/my/xxx/endan/MainActivity.java
+++ app/src/main/java/com/my/xxx/endan/MainActivity.java
@@ -1,11 +1,8 @@
 package com.my.xxx.endan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.my.xxx.mylibrary.EZLedView;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
