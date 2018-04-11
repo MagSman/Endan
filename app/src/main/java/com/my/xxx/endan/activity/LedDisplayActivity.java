866d387ff0dd95a3f88a779ce79381df6d44e9cc
shijianghan <250135506@qq.com>
2018-04-11 13:15:23 +0800

    字幕循环

--- app/src/main/java/com/my/xxx/endan/activity/LedDisplayActivity.java
+++ app/src/main/java/com/my/xxx/endan/activity/LedDisplayActivity.java
@@ -1,5 +1,7 @@
 package com.my.xxx.endan.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -13,61 +15,90 @@ import android.widget.HorizontalScrollView;
 import com.my.xxx.endan.R;
 import com.my.xxx.mylibrary.EZLedView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 //展示LED效果
 public class LedDisplayActivity extends AppCompatActivity {
+
+    @BindView(R.id.scrollView)
+    HorizontalScrollView scrollView;
+    @BindView(R.id.ledView)
+    EZLedView ledView;
+    /* @BindView(R.id.ledViewUp)
+     EZLedView ledViewUp;
+     @BindView(R.id.ledViewDown)
+     EZLedView ledViewDown;*/
+    @BindView(R.id.up)
+    View up;
+    @BindView(R.id.down)
+    View down;
+
     Handler handler = new Handler();
     int scrollX = 0;
     int direct = 1;
+    private StringBuilder builder = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //设置页面没有title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_led_display);
+        ButterKnife.bind(this);
         //隐藏状态栏
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
-
         final int windowWidth = this.getWindowManager().getDefaultDisplay().getWidth();
-        final HorizontalScrollView scrollView = (HorizontalScrollView) findViewById(R.id.scrollView);
-        final EZLedView ledView = (EZLedView) findViewById(R.id.ledView);
-        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) ledView.getLayoutParams();
-      /*  layoutParams.height = ledView.getWidth() + scrollView.getWidth();
-        ledView.setLayoutParams(layoutParams);*/
-     /*   layoutParams.rightMargin = ledView.getWidth() + scrollView.getWidth();
-        ledView.setLayoutParams(layoutParams);*/
-        //ledView.setText("      " + "你好！" + "      ");
-        ledView.setLedRadius(4);
-        ledView.setLedSpace(0);
-        ledView.setDrawable(getResources().getDrawable(R.drawable.simpson));
+        ViewGroup.LayoutParams layoutParamsUp = up.getLayoutParams();
+        layoutParamsUp.width = windowWidth;
+        up.setLayoutParams(layoutParamsUp);
+        ViewGroup.LayoutParams layoutParamsDown = down.getLayoutParams();
+        layoutParamsDown.width = windowWidth;
+        down.setLayoutParams(layoutParamsDown);
+        ledView.setText("你好48964561");
+
         handler.post(new Runnable() {
             @Override
             public void run() {
                 scrollView.scrollTo(scrollX, 0);
                 scrollX += (ledView.getLedRadius() + ledView.getLedSpace()) * direct;
-                if (scrollX <= 0 || scrollX >= ledView.getWidth() - scrollView.getWidth()) {
-                    Log.i("ledView宽度", "---" + ledView.getWidth());
-                    Log.i("scrollView", "---" + scrollView.getWidth());
-                    // direct = -direct;
+                if (scrollX >= (ledView.getWidth() + up.getWidth() + down.getWidth()) - scrollView.getWidth()) {
                     scrollX = (ledView.getLedRadius() + ledView.getLedSpace()) * direct;
-                    Log.i("滚动距离", "---" + scrollX);
-                    Log.i("屏幕宽度", "---" + windowWidth);
                 }
-              /*  if (scrollX >= ledView.getWidth() + scrollView.getWidth()) {
-                    scrollX = (ledView.getLedRadius() + ledView.getLedSpace()) * direct;
-                }*/
+                Log.i("ledView宽度", "---" + ledView.getWidth());
+                Log.i("scrollView宽度", "---" + scrollView.getWidth());
+                Log.i("屏幕宽度", "---" + windowWidth);
                 Log.i("滚动距离", "---" + scrollX);
                 handler.postDelayed(this, 30);
             }
         });
+       /* handler.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.scrollTo(scrollX, 0);
+                scrollX += (ledView.getLedRadius() + ledView.getLedSpace()) * direct;
+                if (scrollX <= 0 || scrollX >= ledView.getWidth() - scrollView.getWidth()) {
+                    scrollX = (ledView.getLedRadius() + ledView.getLedSpace()) * direct;
+                    // direct = -direct;
+                    Log.i("ledView宽度", "---" + ledView.getWidth());
+                    Log.i("scrollView", "---" + scrollView.getWidth());
+                    Log.i("滚动距离", "---" + scrollX);
+                    Log.i("屏幕宽度", "---" + windowWidth);
+                }
+                Log.i("ledView宽度", "---" + ledView.getWidth());
+                Log.i("scrollView宽度", "---" + scrollView.getWidth());
+                Log.i("屏幕宽度", "---" + windowWidth);
+                Log.i("滚动距离", "---" + scrollX);
+                handler.postDelayed(this, 30);
+            }
+        });*/
+    }
 
+    public static void startIntent(Context context) {
+        Intent intent = new Intent(context, LedDisplayActivity.class);
+        context.startActivity(intent);
     }
 }
--- app/src/main/java/com/my/xxx/endan/activity/MainActivity.java
+++ app/src/main/java/com/my/xxx/endan/activity/MainActivity.java
@@ -2,15 +2,21 @@ package com.my.xxx.endan.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.my.xxx.endan.R;
 import com.my.xxx.mylibrary.EZLedView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @BindView(R.id.start)
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +26,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @OnClick({R.id.start})
+    public void Click(View view) {
+        switch (view.getId()) {
+            case R.id.start:
+                LedDisplayActivity.startIntent(this);
+                break;
+        }
+    }
+
+
 }
